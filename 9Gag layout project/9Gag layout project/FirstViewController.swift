54f9963f65fb9061b1a0a6c1021d96d03448f3a6
Peter Jenkin <peternjenkin@gmail.com>
2019-04-16 14:44:05 +0100

    19-193 Button icons on app viewcontroller screens

--- 9Gag layout project/9Gag layout project/FirstViewController.swift
+++ 9Gag layout project/9Gag layout project/FirstViewController.swift
@@ -8,11 +8,16 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,22 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+        
+    // also added to make height of prototype cells ok ()
+    // set height of prototype cell programmatically using delegate function https://stackoverflow.com/a/46438939
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 400
+    }
 
+    
 }
 
